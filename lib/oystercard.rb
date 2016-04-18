fade47d2b25866e1a69695fe61b47df2afea76bb
chriselevate <coates.c@hotmail.com>
2016-04-18 18:33:50 +0100

    06_maximum_balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,16 @@
 class Oystercard
 
+  LIMIT = 90
+
   attr_reader :balance
 
   def initialize
     @balance = 0
   end
 
-  def top_up(balance)
-    @balance += balance
+  def top_up(amount)
+    fail "Maximum limit exceeded by #{(balance + amount) - LIMIT}!" if balance + amount > LIMIT
+    @balance += amount
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -9,4 +9,11 @@ describe Oystercard do
     expect{ subject.top_up 30 }.to change{ subject.balance }.by 30
   end
 
+  it 'raises and error if top up amount increase balance past £90' do
+    expect{ subject.top_up (91) }.to raise_error "Maximum limit exceeded by 1!"
+  end
+
+
+# Use test-driven development approach to update the top_up method to raise an error if the action would take the card beyond the limit.
+
 end
