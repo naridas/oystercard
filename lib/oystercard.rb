cf4b6bbfc2e85fd926c87e109a8b6b03f1940895
Amy Nicholson <missamynicholson@gmail.com>
2016-04-19 17:21:15 +0100

    Challenge 12 - adding journey history

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,18 +1,19 @@
 class Oystercard
 
-  	MAXIMUM_BALANCE = 90
-  	MINIMUM_FARE = 1
+  MAXIMUM_BALANCE = 90
+  MINIMUM_FARE = 1
 
-  	attr_reader :balance, :entry_station
+  attr_reader :balance, :entry_station, :journeys
 
-  	def initialize
-    	@balance = 0
-  	end
+  def initialize
+    @balance = 0
+    @journeys = []
+  end
 
-  	def top_up(amount)
-    	fail "Maximum balance of #{MAXIMUM_BALANCE} exceeded" if amount + balance > MAXIMUM_BALANCE
-    	@balance += amount
-  	end
+  def top_up(amount)
+    fail "Maximum balance of #{MAXIMUM_BALANCE} exceeded" if amount + balance > MAXIMUM_BALANCE
+    @balance += amount
+  end
 
 	def in_journey?
   	!!entry_station
@@ -22,10 +23,18 @@ class Oystercard
 		fail "Need one £1 on card" if balance < MINIMUM_FARE
     @entry_station = station
 	end
-	def touch_out
+
+	def touch_out(exit_station)
 		deduct(MINIMUM_FARE)
+    record(exit_station)
     @entry_station = nil
 	end
+
+  def record(exit_station)
+    hash = {}
+    hash[entry_station] = exit_station
+    @journeys << hash
+  end
 
 	private
 
@@ -34,18 +43,3 @@ class Oystercard
 	end
 
 end
-
-
-
-
-
-
-
-# class Oystercard
-# 	attr_reader :balance
-# 	#DEFAULT_BALANCE = 0
-# 	def intialize#(balance = DEFAULT_BALANCE)
-# 		@balance = 0
-# 	end
-
-# end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,6 +4,7 @@ require 'oystercard'
 describe Oystercard do
 	subject(:oystercard) {described_class.new}
   let(:station) { double(:station) }
+  let(:exit_station) { double(:exit_station) }
 	let(:minimum_fare) {Oystercard::MINIMUM_FARE}
   let(:oyster_topped) do
     oystercard = Oystercard.new
@@ -13,7 +14,6 @@ describe Oystercard do
   end
 
 	describe '#top_up' do
-
   	it 'can top up the balance' do
     	expect{ oystercard.top_up 1 }.to change{ oystercard.balance }.by 1
     end
@@ -32,30 +32,32 @@ describe Oystercard do
   			oystercard.top_up(minimum_fare-1)
   			expect{ oystercard.touch_in(station)}.to raise_error "Need one £1 on card"
 			end
-      # it 'remembers entry station when touch in' do
-      #  oystercard.top_up(minimum_fare)
-      #  expect(oystercard.touch_in(station)).to eq station
-      # end
       it 'remembers entry_station when touch_in' do
         expect(oyster_topped.entry_station).to eq station
   		end
-  		# it 'is initially not in a journey' do
-  		# 	expect(oystercard).not_to be_in_journey
-      # end
-		end
-
-		context '#touch_out' do
-			it "can touch out" do
-  			oyster_topped.touch_out
-  			expect(oyster_topped).not_to be_in_journey
-			end
-			it 'raises an error if the £1 not on card' do
-  			expect{ oyster_topped.touch_out}.to change{ oyster_topped.balance }.by -minimum_fare
-			end
-      it 'forgets entry station on touch_out' do
-        oyster_topped.touch_out
-        expect(oyster_topped.entry_station).to eq nil
-      end
 		end
   end
+
+	context '#touch_out' do
+		it "can touch out" do
+  		oyster_topped.touch_out(exit_station)
+  		expect(oyster_topped).not_to be_in_journey
+		end
+		it 'raises an error if the £1 not on card' do
+  		expect{ oyster_topped.touch_out(exit_station)}.to change{ oyster_topped.balance }.by -minimum_fare
+		end
+    it 'forgets entry station on touch_out' do
+      oyster_topped.touch_out(exit_station)
+      expect(oyster_topped.entry_station).to eq nil
+    end
+  end
+
+  context 'complete journeys' do
+    it 'add the journey to journeys' do
+      oyster_topped.touch_out(exit_station)
+      journey = {}
+      journey[station] = exit_station
+      expect(oyster_topped.journeys).to include journey
+    end
+	end
 end
